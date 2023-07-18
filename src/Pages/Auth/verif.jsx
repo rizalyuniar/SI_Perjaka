be59cee8206aee6128f8e72be065e0ae3fc86edb
unknown <rizalyuniar123@gmail.com>
2023-07-18 16:20:32 +0700

    verif

--- src/Pages/Auth/verif.jsx
+++ src/Pages/Auth/verif.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import style from "./style.module.css";
 
 const verif = () => {
   return (
-    <>
-      <div className="letter-image">
-        <div className="animated-mail">
-          <div className="back-fold" />
-          <div className="letter">
-            <div className="letter-border" />
-            <div className="letter-title" />
-            <div className="letter-context" />
-            <div className="letter-stamp">
-              <div className="letter-stamp-inner" />
+    <div className="container-fluid vw-100">
+      <div className="">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <div className={`${style.payment} shadow`}>
+              <div className={style.payment_header}>
+                <div className={style.check}>
+                  <i className="fa fa-check" aria-hidden="true"></i>
+                </div>
+              </div>
+              <div className={style.content}>
+                <h1 className="font-weight-bold" style={{ fontSize: "40px" }}>
+                  Verification Success!
+                </h1>
+                <p>
+                  Thanks for keeping Stitch safe! <br /> if you questions you
+                  can contact us any time by emailing.{" "}
+                </p>
+                <Link to="/login" className="text-decoration-none p-2 px-3">
+                  Go to Login
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="top-fold" />
-          <div className="body" />
-          <div className="left-fold" />
         </div>
-        <div className="shadow" />
       </div>
-    </>
+    </div>
   );
 };
 
